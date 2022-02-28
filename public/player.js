b6db86d4af0c52fe37954e926ffe4676636a212b
Peter Shen <shenjiashu@hotmail.com>
2022-02-28 10:48:35 -0800

    player invest and withdraw

--- public/player.js
+++ public/player.js
@@ -1,8 +1,11 @@
 class Player {
-
-    playerNFTs = [];
-    walletBalance = 0;
-    perkTotal = 0;
+    
+    constructor() {
+        this.playerNFTs = [];
+        this.walletBalance = 0;
+        this.perkTotal = 0;
+        this.coins = [new Coin("alpha", 0, 0.5, 2), new Coin("beta", 10, 0.1, 3), new Coin("sigma", 10000, 0.02, 4)];
+    }
 
     getUpdatedBalance() {
 
@@ -17,14 +20,20 @@ class Player {
     }
 
     invest(usd, coinType) {
-
+        coin = coins[coinType];
+        coin.balance += usd;
+        coin.updateNumCoins();
     }
 
     withdraw(usd, coinType) {
-
+        coin = coins[coinType];
+        if (usd < coin.balance) {
+            coin.balance -= usd;
+            coin.updateNumCoins();
+        }
     }
 
     checkPerks() {
-        
+
     }
 }
