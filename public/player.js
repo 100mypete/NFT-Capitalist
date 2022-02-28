0cb5186a3949e884b326e73e8b2ef0cea32e8d0f
Peter Shen <shenjiashu@hotmail.com>
2022-02-28 10:38:57 -0800

    skeleton of player.js

---
+++ public/player.js
@@ -0,0 +1,30 @@
+class Player {
+
+    playerNFTs = [];
+    walletBalance = 0;
+    perkTotal = 0;
+
+    getUpdatedBalance() {
+
+    }
+
+    buyNFT(index) {
+
+    }
+
+    checkOwnedNFTs() {
+
+    }
+
+    invest(usd, coinType) {
+
+    }
+
+    withdraw(usd, coinType) {
+
+    }
+
+    checkPerks() {
+        
+    }
+}
