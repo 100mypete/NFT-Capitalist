f54fc4aa931884e90e2477c6302161089c90c7f7
Justin Sun <justinsunyt@gmail.com>
2022-02-27 16:39:59 -0800

    Marketplace, need to fix ES6 imports

---
+++ public/marketplace.js
@@ -0,0 +1,17 @@
+import NFT from "nft";
+
+class Marketplace {
+
+  constructor() {
+    // Example preset marketplace
+    this.NFTs = [
+      new NFT("cat", 2, 0.01),
+      new NFT("monkey", 69, 0.1),
+      new NFT("horse", 420, 0.3),
+    ];
+  }
+
+  removeNFT(index) {
+    this.NFTs.splice(index, 1);
+  }
+}
