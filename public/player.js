23f5bd2597d0677167ed961c849f5729f0f193c3
solomonbaik <52802497+solomonbaik@users.noreply.github.com>
2022-03-17 15:01:51 -0700

    buying nfts works
    balances also work... i think

--- public/player.js
+++ public/player.js
@@ -1,23 +1,15 @@
 class Player {
   constructor() {
-    if (window.localStorage.getItem("playerNFTs") == null) {
-      window.localStorage.setItem("playerNFTs", JSON.stringify([]));
-    }
-    this.playerNFTs = JSON.parse(window.localStorage.getItem("playerNFTs"));
-    if (window.localStorage.getItem("walletBalance") == null) {
-      window.localStorage.setItem("walletBalance", 1000);
-    }
-    this.walletBalance = window.localStorage.getItem("walletBalance");
+    this.playerNFTs = window.localStorage.getItem("playerNFTs");
+    this.playerNFTs = [];
+    this.walletBalance = 173842069.0;
     this.perkTotal = 0;
-    this.perkTotal = this.checkPerks();
-    if (window.localStorage.getItem("coins") == null) {
-      window.localStorage.setItem("coins", JSON.stringify([new Coin("alpha", 0, 0.5, 2), new Coin("beta", 10, 0.1, 3), new Coin("sigma", 10000, 0.02, 4)]));
-    }
-    this.coins = JSON.parse(window.localStorage.getItem("coins"));
-    if (window.localStorage.getItem("marketplace") == null) {
-      window.localStorage.setItem("marketplace", JSON.stringify(new Marketplace()));
-    }
-    this.marketplace = JSON.parse(window.localStorage.getItem("marketplace"));
+    this.coins = [
+      new Coin("Alpha", 0, 0.5, 2),
+      new Coin("Beta", 10, 0.1, 3),
+      new Coin("Sigma", 10000, 0.02, 4),
+    ];
+    this.marketplace = new Marketplace();
   }
 
   getTotalBalance() {
@@ -36,19 +28,16 @@ class Player {
   getBetaBalance() {
     return this.coins[1].balance;
   }
-  
+
   getSigmaBalance() {
     return this.coins[2].balance;
   }
 
   buyNFT(index) {
-      let nft = this.marketplace.removeNFT(index);
-
-      this.playerNFTs.push(nft);
-      window.localStorage.setItem("playerNFTs", JSON.stringify(this.playerNFTs));
-      this.perkTotal += nft.APRPerk;
-      this.walletBalance -= nft.price;
-      window.localStorage.setItem("walletBalance", this.walletBalance);
+    let nft = this.marketplace.removeNFT(index);
+    this.playerNFTs.push(nft);
+    this.perkTotal += nft.APRPerk;
+    this.walletBalance -= nft.price;
   }
 
   checkOwnedNFTs() {
@@ -56,29 +45,18 @@ class Player {
   }
 
   invest(usd, coinType) {
-    if (usd <= this.walletBalance) {
-      let coin = this.coins[coinType];
-      coin.balance += usd;
-      this.walletBalance -= usd;
-      window.localStorage.setItem("walletBalance", this.walletBalance);
-      coin.updateNumCoins();
-      window.localStorage.setItem("coins", this.coins);
-    } else {
-      console.log("Insufficient wallet balance");
-    }
+    let coin = this.coins[coinType];
+    coin.balance += usd;
+    this.walletBalance -= usd;
+    coin.updateNumCoins();
   }
 
   withdraw(usd, coinType) {
     let coin = this.coins[coinType];
-    if (usd <= coin.balance) {
+    if (usd < coin.balance) {
       coin.balance -= usd;
-            this.walletBalance += usd;
-            window.localStorage.setItem("walletBalance", this.walletBalance);
-            coin.updateNumCoins();
-            window.localStorage.setItem("coins", this.coins);
-      coin.harvestAPR();
-    } else {
-      console.log("Insufficient coin balance");
+      this.walletBalance += usd;
+      coin.updateNumCoins();
     }
   }
 
