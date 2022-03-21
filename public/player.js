b422066fedf4f3a3291bdfdf985ef67bcb9722b9
Justin Sun <justinsunyt@gmail.com>
2022-03-21 11:14:44 -0700

    Fixed coin localStorage

--- public/player.js
+++ public/player.js
@@ -7,78 +7,104 @@ class Player {
     if (window.localStorage.getItem("walletBalance") == null) {
       window.localStorage.setItem("walletBalance", 1000);
     }
-    this.walletBalance = window.localStorage.getItem("walletBalance");
+    this.walletBalance = parseFloat(
+      window.localStorage.getItem("walletBalance")
+    );
     this.perkTotal = this.checkPerks();
 
     //coin stuff
-    this.coins = [new Coin("alpha", 0, 0.5, 2), new Coin("beta", 10, 0.1, 3), new Coin("sigma", 10000, 0.02, 4)];
+    this.coins = [
+      new Coin("alpha", 0, 0.5, 2),
+      new Coin("beta", 10, 0.1, 3),
+      new Coin("sigma", 100, 0.02, 4),
+    ];
 
     //alpha
     if (window.localStorage.getItem("alphaBalance") == null) {
       window.localStorage.setItem("alphaBalance", 0.0);
     }
-    this.coins[0].balance = window.localStorage.getItem("alphaBalance");
+    this.coins[0].balance = parseFloat(
+      window.localStorage.getItem("alphaBalance")
+    );
 
     if (window.localStorage.getItem("alphaNumCoins") == null) {
       window.localStorage.setItem("alphaNumCoins", 0.0);
     }
-    this.coins[0].numCoins = window.localStorage.getItem("alphaNumCoins");
+    this.coins[0].numCoins = parseFloat(
+      window.localStorage.getItem("alphaNumCoins")
+    );
 
     if (window.localStorage.getItem("alphaPrice") == null) {
       window.localStorage.setItem("alphaPrice", 1000.0);
     }
-    this.coins[0].price = window.localStorage.getItem("alphaPrice");
+    this.coins[0].price = parseFloat(window.localStorage.getItem("alphaPrice"));
 
     if (window.localStorage.getItem("alphaRewards") == null) {
       window.localStorage.setItem("alphaRewards", 0.0);
     }
-    this.coins[0].rewards = window.localStorage.getItem("alphaRewards");
+    this.coins[0].rewards = parseFloat(
+      window.localStorage.getItem("alphaRewards")
+    );
 
     //beta
     if (window.localStorage.getItem("betaBalance") == null) {
       window.localStorage.setItem("betaBalance", 0.0);
     }
-    this.coins[1].balance = window.localStorage.getItem("betaBalance");
+    this.coins[1].balance = parseFloat(
+      window.localStorage.getItem("betaBalance")
+    );
 
     if (window.localStorage.getItem("betaNumCoins") == null) {
       window.localStorage.setItem("betaNumCoins", 0.0);
     }
-    this.coins[1].numCoins = window.localStorage.getItem("betaNumCoins");
+    this.coins[1].numCoins = parseFloat(
+      window.localStorage.getItem("betaNumCoins")
+    );
 
     if (window.localStorage.getItem("betaPrice") == null) {
       window.localStorage.setItem("betaPrice", 1000.0);
     }
-    this.coins[1].price = window.localStorage.getItem("betaPrice");
+    this.coins[1].price = parseFloat(window.localStorage.getItem("betaPrice"));
 
     if (window.localStorage.getItem("betaRewards") == null) {
       window.localStorage.setItem("betaRewards", 0.0);
     }
-    this.coins[1].rewards = window.localStorage.getItem("betaRewards");
+    this.coins[1].rewards = parseFloat(
+      window.localStorage.getItem("betaRewards")
+    );
 
     //sigma
     if (window.localStorage.getItem("sigmaBalance") == null) {
       window.localStorage.setItem("sigmaBalance", 0.0);
     }
-    this.coins[2].balance = window.localStorage.getItem("sigmaBalance");
+    this.coins[2].balance = parseFloat(
+      window.localStorage.getItem("sigmaBalance")
+    );
 
     if (window.localStorage.getItem("sigmaNumCoins") == null) {
       window.localStorage.setItem("sigmaNumCoins", 0.0);
     }
-    this.coins[2].numCoins = window.localStorage.getItem("sigmaNumCoins");
+    this.coins[2].numCoins = parseFloat(
+      window.localStorage.getItem("sigmaNumCoins")
+    );
 
     if (window.localStorage.getItem("sigmaPrice") == null) {
       window.localStorage.setItem("sigmaPrice", 1000.0);
     }
-    this.coins[2].price = window.localStorage.getItem("sigmaPrice");
+    this.coins[2].price = parseFloat(window.localStorage.getItem("sigmaPrice"));
 
     if (window.localStorage.getItem("sigmaRewards") == null) {
       window.localStorage.setItem("sigmaRewards", 0.0);
     }
-    this.coins[2].rewards = window.localStorage.getItem("sigmaRewards");
+    this.coins[2].rewards = parseFloat(
+      window.localStorage.getItem("sigmaRewards")
+    );
 
-    
     if (window.localStorage.getItem("marketplace") == null) {
-      window.localStorage.setItem("marketplace", JSON.stringify(new Marketplace()));
+      window.localStorage.setItem(
+        "marketplace",
+        JSON.stringify(new Marketplace())
+      );
     }
     this.marketplace = JSON.parse(window.localStorage.getItem("marketplace"));
   }
@@ -109,6 +135,11 @@ class Player {
     this.playerNFTs.push(nft);
     this.perkTotal += nft.APRPerk;
     this.walletBalance -= nft.price;
+    window.localStorage.setItem(
+      "marketplace",
+      JSON.stringify(this.marketplace)
+    );
+    window.localStorage.setItem("playerNFTs", JSON.stringify(this.playerNFTs));
   }
 
   checkOwnedNFTs() {
@@ -119,22 +150,8 @@ class Player {
     if (usd <= this.walletBalance) {
       let coin = this.coins[coinType];
       coin.balance += usd;
-      switch (coinType) {
-        case 0:
-          window.localStorage.setItem("alphaBalance", this.coins[0].balance);
-          break;
-        case 1:
-          window.localStorage.setItem("betaBalance", this.coins[1].balance);
-          break;
-        case 2:
-          window.localStorage.setItem("sigmaBalance", this.coins[2].balance);
-          break;
-      }
-
       this.walletBalance -= usd;
-      window.localStorage.setItem("walletBalance", this.walletBalance);
       coin.updateNumCoins();
-      window.localStorage.setItem("coins", this.coins);
     } else {
       console.log("Insufficient wallet balance");
     }
@@ -144,22 +161,8 @@ class Player {
     let coin = this.coins[coinType];
     if (usd < coin.balance) {
       coin.balance -= usd;
-      switch (coinType) {
-        case 0:
-          window.localStorage.setItem("alphaBalance", this.coins[0].balance);
-          break;
-        case 1:
-          window.localStorage.setItem("betaBalance", this.coins[1].balance);
-          break;
-        case 2:
-          window.localStorage.setItem("sigmaBalance", this.coins[2].balance);
-          break;
-      }
-
       this.walletBalance += usd;
-      window.localStorage.setItem("walletBalance", this.walletBalance);
       coin.updateNumCoins();
-      window.localStorage.setItem("coins", this.coins);
       coin.harvestAPR();
     } else {
       console.log("Insufficient coin balance");
